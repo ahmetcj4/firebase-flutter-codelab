7b4e7e041d77551941df9b3792fde6953a302d74
Ahmet Zorer <ahmetcj4@hotmail.com>
2018-02-11 02:56:54 +0300

    send image feature is implemented

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,11 @@ import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_database/ui/firebase_animated_list.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:image_picker/image_picker.dart';
+
+import 'dart:math';
+import 'dart:io';
 
 final googleSignIn = new GoogleSignIn();
 final analytics = new FirebaseAnalytics();
@@ -60,9 +65,10 @@ class ChatScreenState extends State<ChatScreen> {
     _sendMessage(text: text);
   }
 
-  void _sendMessage({String text}) {
+  void _sendMessage({String text, String imageUrl}) {
     reference.push().set({
       'text': text,
+      'imageUrl': imageUrl,
       'senderName': googleSignIn.currentUser.displayName,
       'senderPhotoUrl': googleSignIn.currentUser.photoUrl,
     });
@@ -80,6 +86,22 @@ class ChatScreenState extends State<ChatScreen> {
       margin: const EdgeInsets.symmetric(horizontal: 8.0),
       child: new Row(
         children: <Widget>[
+          new Container(
+            margin: new EdgeInsets.symmetric(horizontal: 4.0),
+            child: new IconButton(
+              icon: new Icon(Icons.photo_camera),
+              onPressed: () async {
+                await _ensureLoggedIn();
+                File imageFile = await ImagePicker.pickImage();
+                int random = new Random().nextInt(100000);
+                StorageReference ref =
+                    FirebaseStorage.instance.ref().child("image_$random.jpg");
+                StorageUploadTask uploadTask = ref.put(imageFile);
+                Uri downloadUrl = (await uploadTask.future).downloadUrl;
+                _sendMessage(imageUrl: downloadUrl.toString());
+              },
+            ),
+          ),
           new Flexible(
             child: new TextField(
               controller: _textController,
@@ -177,8 +199,7 @@ class ChatMessage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new SizeTransition(
-      sizeFactor: new CurvedAnimation(
-          parent: animation, curve: Curves.easeOut),
+      sizeFactor: new CurvedAnimation(parent: animation, curve: Curves.easeOut),
       axisAlignment: 0.0,
       child: new Container(
         margin: const EdgeInsets.symmetric(vertical: 10.0),
@@ -200,7 +221,14 @@ class ChatMessage extends StatelessWidget {
                       style: Theme.of(context).textTheme.subhead),
                   new Container(
                     margin: const EdgeInsets.only(top: 5.0),
-                    child: new Text(snapshot.value['text']),
+                    child: snapshot.value['imageUrl'] != null
+                        ? new Container(
+                            child: new Image.network(
+                              snapshot.value['imageUrl'],
+                              width: 250.0,
+                            ),
+                          )
+                        : new Text(snapshot.value['text']),
                   ),
                 ],
               ),
